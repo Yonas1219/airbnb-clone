91ba51ffa5808cf746982cd972ed9f325da44b85
Yonas Enchalew <yonasenchalew@gmail.com>
2023-06-07 23:38:39 +0300

    changes

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -16,3 +16,5 @@ const Logo = () => {
         />
     )
 }
+
+export default Logo;
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,16 +1,20 @@
 import Container from "../Container";
+import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
 const Navbar = () => {
-    return (
-        <div className="fixed w-full bg-white z-10 shadow-sm">
-          <div className="py-4 border-b-[1px]">
-            <Container>
-                <div
-                className="flex flex-row items-center justify-between gap-3 md:gap-0">
-                    < Logo />
-                </div>
-            </Container>
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+            <Logo />
+            <Search />
+            <UserMenu />
           </div>
-        </div>
-    )
-}
+        </Container>
+      </div>
+    </div>
+  );
+};
 export default Navbar;
